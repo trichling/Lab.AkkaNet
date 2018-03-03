dfb2ce10d74047cf15aeb3df3ae413b44d86d1cd
Tobias Richling <trichling@gmx.de>
2018-03-03 21:19:41 +0100

    Add project files.

---
+++ Lab.AkkaNet.Banking.Vanilla/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab.AkkaNet.Banking.Vanilla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
